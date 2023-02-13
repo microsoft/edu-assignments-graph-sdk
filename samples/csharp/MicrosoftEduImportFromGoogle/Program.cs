61de9c765bd48eebc7a3acfd6b8a01d0cdfe7fac
Bivas Maiti <bivasmaiti26@gmail.com>
2023-02-13 23:00:55 +0000

    Initial Commit for Import From Google

---
+++ samples/csharp/MicrosoftEduImportFromGoogle/Program.cs
@@ -0,0 +1,10 @@
+﻿using MicrosoftEduGraphSamples.Workflows;
+using Microsoft.Extensions.Configuration;
+
+// See https://aka.ms/new-console-template for more information
+
+// Build configuration
+IConfiguration config = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json", true, true)
+    .Build();
