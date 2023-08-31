0a7394de174b5db1c6d49fec6eee9d25e291d23f
raghuchek <v-rchekuri@microsoft.com>
2023-08-31 22:26:11 +0530

    Modules workflow

---
+++ samples/csharp/MicrosoftEduGraphSamples/Workflows/ModuleWorkflow.cs
@@ -0,0 +1,150 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Graph.Beta;
+using Microsoft.Graph.Beta.Models;
+using MicrosoftGraphSDK;
+using System.Resources;
+
+namespace MicrosoftEduGraphSamples.Workflows
+{
+    /// <summary>
+    /// Contains all the workflows related to Modules
+    /// </summary>
+    internal class ModuleWorkflow
+    {
+        private readonly IConfiguration _config;
+
+        public ModuleWorkflow(IConfiguration configuration)
+        {
+            this._config = configuration;
+
+            // Verify and throw exception for input values if null or empty
+            try
+            {
+                if (string.IsNullOrEmpty(_config["classId"]))
+                {
+                    throw new Exception("Missing classId please check appconfig.json file.");
+                }
+                else if (string.IsNullOrEmpty(_config["tenantId"]))
+                {
+                    throw new Exception("Missing tenantId please check appconfig.json file.");
+                }
+                else if (string.IsNullOrEmpty(_config["secret"]))
+                {
+                    throw new Exception("Missing secret please check appconfig.json file.");
+                }
+                else if (string.IsNullOrEmpty(_config["appId"]))
+                {
+                    throw new Exception("Missing appId please check appconfig.json file.");
+                }
+                else if (string.IsNullOrEmpty(_config["teacherAccount"]))
+                {
+                    throw new Exception("Missing teacherAccount please check appconfig.json file.");
+                }
+                else if (string.IsNullOrEmpty(_config["studentAccount"]))
+                {
+                    throw new Exception("Missing studentAccount please check appconfig.json file.");
+                }
+                else if (string.IsNullOrEmpty(_config["password"]))
+                {
+                    throw new Exception("Missing password please check appconfig.json file.");
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+        /// <summary>
+        /// Workflow to create and publish the module
+        /// </summary>
+        public async Task ClassworkAsync()
+        {
+            try
+            {
+                // Get a Graph client using delegated permissions
+                var graphClient = GraphClient.GetDelegateClient(_config["tenantId"], _config["appId"], _config["teacherAccount"], _config["password"]);
+
+                //1 - create a draft module.
+                var module = await MicrosoftGraphSDK.Module.CreateAsync(graphClient, _config["classId"], "Module Workflow", "This is the first module created using Graph SDK.");
+                Console.WriteLine($"New module has been created: {module.Id} - {module.DisplayName} - {module.Status}");
+                await MicrosoftGraphSDK.Module.SetupResourcesFolder(graphClient, _config["classId"], module.Id);
+
+                //2 - add a link resource.
+                EducationModuleResource requestBody = new EducationModuleResource
+                {
+                    Resource = new EducationLinkResource
+                    {
+                        OdataType = "#microsoft.graph.educationLinkResource",
+                        DisplayName = "Bing site",
+                        Link = "https://www.bing.com",
+                    },
+                };
+                // A new file is added
+                var newResource = await MicrosoftGraphSDK.Module.PostResourceAsync(graphClient, _config["classId"], module.Id.ToString(), requestBody);
+
+                //3 - add a new word document resource.
+                requestBody = new EducationModuleResource
+                {
+                    Resource = new EducationWordResource
+                    {
+                        OdataType = "#microsoft.graph.educationWordResource",
+                        DisplayName = "test_word_file.docx",
+
+                    },
+                };
+                // A new Word file is added
+                newResource = await MicrosoftGraphSDK.Module.PostResourceAsync(graphClient, _config["classId"], module.Id.ToString(), requestBody);
+
+                //5 - add a channel resource
+                requestBody = new EducationModuleResource
+                {
+                    Resource = new EducationChannelResource
+                    {
+                        OdataType = "#microsoft.graph.educationChannelResource",
+                        Url = "https://graph.microsoft.com/v1.0/teams/" + _config["classId"] + "/channels/19:Z2JOIPdvU-i1gymVObk9KhJj_sZCvDFEyGXPO7jLLUA1@thread.tacv2",
+                        DisplayName = "General",
+                    },
+                };
+                // Added a channel resource
+                newResource = await MicrosoftGraphSDK.Module.PostResourceAsync(graphClient, _config["classId"], module.Id.ToString(), requestBody);
+
+                //7 - assignment resource
+                var assignment = await MicrosoftGraphSDK.Assignment.CreateAsync(graphClient, _config["classId"]);
+                var assignmentId = assignment.Id;
+                requestBody = new EducationModuleResource
+                {
+                    Resource = new EducationLinkedAssignmentResource
+                    {
+                        OdataType = "#microsoft.graph.educationLinkedAssignmentResource",
+                        Url = "https://graph.microsoft.com/v1.0/education/classes/" + _config["classId"] + "/assignments/"+assignmentId,
+                    },
+                };
+                // Added a assignment resource
+                newResource = await MicrosoftGraphSDK.Module.PostResourceAsync(graphClient, _config["classId"], module.Id.ToString(), requestBody);
+
+                //8 - publish the module.
+                module = await MicrosoftGraphSDK.Module.PublishAsync(graphClient, _config["classId"], module.Id.ToString());
+
+                //9 - switch to student account
+                graphClient = GraphClient.GetDelegateClient(_config["tenantId"], _config["appId"], _config["studentAccount"], _config["password"]);
+
+                //10 - get all module resources and log in the sample. (student).
+                var resources = await MicrosoftGraphSDK.Module.GetModuleResourcesAsync(graphClient, _config["classId"], module.Id.ToString());
+
+                // Iterate over all the resources values
+                foreach ( var resource in resources.Value )
+                {
+                    Console.WriteLine($"Resource: {resource.Resource.DisplayName}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"ClassworkAsync: {ex.ToString()}");
+                return;
+            }
+        }
+    }
+}
---
+++ samples/csharp/MicrosoftGraphSDK/Module.cs
@@ -0,0 +1,112 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Microsoft.Graph.Beta;
+using Microsoft.Graph.Beta.Models;
+
+namespace MicrosoftGraphSDK
+{
+    /// <summary>
+    /// Graph SDK endpoints for modules
+    /// </summary>
+    public class Module
+    {
+        public static async Task<EducationModule> CreateAsync(
+            GraphServiceClient client,
+            string classId,
+            string displayName,
+            string description)
+        {
+            try
+            {
+                var requestBody = new EducationModule
+                {
+                    DisplayName = displayName,
+                    Description = description,
+                };
+
+                return await client.Education
+                    .Classes[classId]
+                    .Modules.PostAsync(requestBody);
+            }
+            catch (Exception ex) {
+                throw new GraphException($"CreateAsync call: {ex.Message}", ex, classId);
+            }
+        }
+
+        public static async Task<EducationModuleResource> PostResourceAsync(
+            GraphServiceClient client,
+            string classId,
+            string moduleId,
+            EducationModuleResource resource)
+        {
+            try
+            {
+                return await client.Education
+                    .Classes[classId]
+                    .Modules[moduleId]
+                    .Resources.PostAsync(resource);
+            }
+            catch (Exception ex)
+            {
+                throw new GraphException($"PostResourceAsync call: {ex.Message}", ex, classId);
+            }
+        }
+
+        public static async Task<EducationModule> PublishAsync(
+            GraphServiceClient client,
+            string classId,
+            string moduleId)
+        {
+            try
+            {
+                return await client.Education
+                    .Classes[classId]
+                    .Modules[moduleId]
+                    .Publish
+                    .PostAsync();
+            }
+            catch (Exception ex)
+            {
+                throw new GraphException($"PublishAsync call: {ex.Message}", ex, classId);
+            }
+        }
+
+        public static async Task<EducationModuleResourceCollectionResponse> GetModuleResourcesAsync(
+            GraphServiceClient client,
+            string classId,
+            string moduleId)
+        {
+            try
+            {
+                return await client.Education
+                    .Classes[classId]
+                    .Modules[moduleId]
+                    .Resources.GetAsync();
+            }
+            catch (Exception ex)
+            {
+                throw new GraphException($"GetModuleResourcesAsync call: {ex.Message}", ex, classId);
+            }
+        }
+        public static async Task<EducationModule> SetupResourcesFolder(
+            GraphServiceClient client,
+            string classId,
+            string moduleId)
+        {
+            try
+            {
+                return await client.Education
+                    .Classes[classId]
+                    .Modules[moduleId]
+                    .SetUpResourcesFolder
+                    .PostAsync();
+            }
+            catch (Exception ex)
+            {
+                throw new GraphException($"SetupResourcesFolder call: {ex.Message}", ex, classId);
+            }
+        }
+
+    }
+}
