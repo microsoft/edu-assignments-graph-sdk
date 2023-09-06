443f127fcc1c62fe99f8e8892daff7b9ca69a082
Cristobal Buenrostro Lopez (TECH MAHINDRA LIMITED) <v-bcristobal@microsoft.com>
2023-09-06 15:45:44 -0700

    brief description to class and methods for export.cs

--- samples/csharp/MicrosoftEduImportFromGoogle/Export.cs
+++ samples/csharp/MicrosoftEduImportFromGoogle/Export.cs
@@ -4,6 +4,9 @@ using System.Text.Json;
 
 namespace MicrosoftEduImportFromGoogle
 {
+    /// <summary>
+    /// Google endpoints needed to export classroom courseWork and courseWorkMaterials
+    /// </summary>
     internal class Export
     {
         private readonly IConfiguration _config;
@@ -12,13 +15,20 @@ namespace MicrosoftEduImportFromGoogle
         {
             this._config = configuration;
         }
+
+        /// <summary>
+        /// Authorizes the application and sets the Google access token
+        /// </summary>
+        /// <returns>Course[]</returns>
         public async Task AuthorizeApp()
         {
             this.accessToken = await GoogleAuthenticator.AuthorizeAppAndGetTokenFromGoogle(_config["googleClientId"], _config["googleClientSecret"], _config["googleAuthEndpoint"]);
         }
 
-        // client configuration
-
+        /// <summary>
+        /// Returns an array of courses that the requesting user is permitted to view
+        /// </summary>
+        /// <returns>Course[]</returns>
         public async Task<Course[]?> GetCourses()
         {
             List<string> courseIds= new List<string>();
@@ -31,6 +41,11 @@ namespace MicrosoftEduImportFromGoogle
             return (courseDictionary == null) ? null : courseDictionary?["courses"];
         }
 
+        /// <summary>
+        /// Returns an array of draft and published course work that the requester is permitted to view
+        /// </summary>
+        /// <param name="course">Course</param>
+        /// <returns>CourseWork[]</returns>
         public async Task<CourseWork[]?> GetCourseWork(Course course)
         {
 			Console.WriteLine("* Getting coursework for course [{0}] from Google Classroom...", course.Name);
@@ -44,6 +59,11 @@ namespace MicrosoftEduImportFromGoogle
             return (courseWorkDictionary == null) ? null : courseWorkDictionary["courseWork"];
         }
 
+        /// <summary>
+        /// Returns an array of course work material that the requester is permitted to view.
+        /// </summary>
+        /// <param name="courseId">Course id</param>
+        /// <returns>CourseWorkMaterials[]</returns>
         public async Task<CourseWorkMaterials[]> GetCourseWorkMaterials(string courseId)
         {
             Console.WriteLine("Course Materials");
@@ -58,6 +78,11 @@ namespace MicrosoftEduImportFromGoogle
             return courseWorkMaterialDictionary["courseWorkMaterial"];
         }
 
+        /// <summary>
+        /// Returns a file metadata from Google Drive
+        /// </summary>
+        /// <param name="fileId">File id</param>
+        /// <returns>Dictionary<string, string></returns>
         public async Task<Dictionary<string, string>> GetGoogleDriveFileMetadata(string fileId)
         {
             string url = $"https://www.googleapis.com/drive/v3/files/{fileId}";
@@ -65,12 +90,18 @@ namespace MicrosoftEduImportFromGoogle
             return fileDetails;
         }
 
+        /// <summary>
+        /// Exports Google Drive file to target mime type
+        /// </summary>
+        /// <param name="fileId">File id</param>
+        /// <param name="targetMimeType">Target mime type</param>
+        /// <param name="export">Flag to export file, default is false</param>
+        /// <returns>Byte[]</returns>
         public async Task<Byte[]> GetGoogleDoc(string fileId, string targetMimeType, bool export = false)
         {
             string query = export ? $"/export?mimeType={targetMimeType}" : "?alt=media";
             string url = $"https://www.googleapis.com/drive/v3/files/{fileId}{query}";
             return await Utilities.MakeHttpGetByteArrayRequest(accessToken, url);
         }
-
     }
 }
