b2514eb3efe0de14ebf344e3d7a820fd491fae1e
R Prashanth Kumar Manda <v-rmanda@microsoft.com>
2024-02-13 18:28:10 +0530

    Added parameter description

--- samples/csharp/MicrosoftEduGraphSamples/Workflows/ModuleWorkflow.cs
+++ samples/csharp/MicrosoftEduGraphSamples/Workflows/ModuleWorkflow.cs
@@ -25,6 +25,7 @@ namespace MicrosoftEduGraphSamples.Workflows
         /// <summary>
         /// Workflow to create and publish the module
         /// </summary>
+        /// <param name="appOnly">True value authenticates the graph client with application permissions only, otherwise it will be created with delegated permissions</param> 
         public async Task ClassworkAsync(bool appOnly = false)
         {
             try
