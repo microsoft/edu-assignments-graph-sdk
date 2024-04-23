a1ef482ed825d8ac8e462c9fed9ddfb6bc3898c4
v-rmanda <147627908+v-rmanda@users.noreply.github.com>
2024-04-23 09:32:40 +0530

    Update samples/csharp/MicrosoftEduGraphSamples/Program.cs
    Co-authored-by: cristobal-buenrostro <87383938+cristobal-buenrostro@users.noreply.github.com>

--- samples/csharp/MicrosoftEduGraphSamples/Program.cs
+++ samples/csharp/MicrosoftEduGraphSamples/Program.cs
@@ -15,5 +15,5 @@ IConfiguration config = new ConfigurationBuilder()
 // Create an instance of the class you want to test and call the desired workflow method.
 // Each flow represents a common entire process that can be tested using Microsoft Graph SDK, all the sample flows are located in the "workflows" folder.
 
-SubmissionWorkflow workflow2 = new SubmissionWorkflow(config);
-await workflow2.SubmissionFeedbackResource();
+SubmissionWorkflow workflow = new SubmissionWorkflow(config);
+await workflow.SubmissionFeedbackResource();
